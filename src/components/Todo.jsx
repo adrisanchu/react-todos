0a766849e2bf388bbcc8e981ba3bb6eaee48572d
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-02-03 19:12:00 +0100

    Added handler to change status of the todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -7,11 +7,28 @@ const Todo = ({ todo, todos, setTodos }) => {
     // a bit weird to need access to all todos from a single todo though...
     setTodos(todos.filter((el) => el.id !== todo.id));
   };
+
+  const completeHandler = () => {
+    setTodos(
+      todos.map((item) => {
+        // go through all todos and check if completed is set to true or false
+        if (item.id === todo.id) {
+          return {
+            // return all items changing the completed property to the opposite of the previous !
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      })
+    );
+  };
+
   const { text } = todo;
   return (
     <div className="todo">
       <li className="todo-item">{text}</li>
-      <button className="complete-btn">
+      <button onClick={completeHandler} className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
       <button onClick={deleteHandler} className="garbage-btn">
