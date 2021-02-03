7027007bbca63cba9316038a11e159b3e5e443cf
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-02-03 17:23:51 +0100

    submit Form functionality

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,20 +1,32 @@
 import React from "react";
 
-const Form = ({ setInputText }) => {
+const Form = ({ todos, setTodos, inputText, setInputText }) => {
   const inputTextHandler = (e) => {
-    // get the input text from the form
+    // get the input text from the form via event
     setInputText(e.target.value);
   };
-
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      {
+        text: inputText,
+        completed: false,
+        id: Math.floor(Math.random() * 1000),
+      },
+    ]);
+    setInputText("");
+  };
   return (
     <form>
       <input
+        value={inputText}
         onChange={inputTextHandler}
         type="text"
         placeholder="Type something..."
         className="todo-input"
       />
-      <button className="todo-button" type="submit">
+      <button onClick={submitTodoHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
     </form>
